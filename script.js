b4547886eb3e550051c749dbaf5a143d3742f7a1
PragyeshChauhan <146080551+PragyeshChauhan@users.noreply.github.com>
2024-02-01 00:13:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+
+let project = document.querySelectorAll('.working');
+
+project.forEach((value)=>{
+    value.addEventListener('click' ,function(event){
+        console.log(event.target);
+        event.preventDefault();
+        alert('working on this Project ')   
+    },false)
+})
